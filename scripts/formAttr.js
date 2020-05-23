a81e41529d2919aef596fb453b606e09c17f6271
Jared Grounds <groundsy59@gmail.com>
2020-05-23 15:03:42 -0400

    Stylesheet fixes

--- scripts/formAttr.js
+++ scripts/formAttr.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     $("#form-message").on("input", function(){
         var charCount = $("#form-message").val().length;
-        $("#charcount").html("Characters left: " + (255 - charCount));
+        $("#charcount").html("Characters left: " + (256 - charCount));
 
     })
 })
